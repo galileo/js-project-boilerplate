f2175f4b650fe30eef06b5d49fa9a9670bf4aeda
Kamil Ronewicz <galileox86@gmail.com>
2017-05-29 13:39:05 +0200

    Initilaize webpack and babel

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# JavaScript project boilerplate
+
+## Install project
+
+yarn install
+
+## Build project
+
+yarn run webpack
+
+## Project testing
+
+yarn run test
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: [
+    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
+    './src/App.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, './node_modules/react'),
+      React: path.resolve(__dirname, './node_modules/react')
+    },
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpg|)$/,
+        loader: 'url-loader?limit=200000'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.woff(2)?(\?[a-z0-9]+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      }, {
+        test: /\.(ttf|eot|svg)(\?[a-z0-9]+)?$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ]
+}
