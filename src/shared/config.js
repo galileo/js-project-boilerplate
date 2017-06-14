abd38462c3a8f2282aab06372f129b06e2f34882
Kamil Ronewicz <galileox86@gmail.com>
2017-06-14 13:18:36 +0200

    Change application name

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,3 +1,3 @@
 export const WEB_PORT = process.env.PORT || 8000
 export const STATIC_PATH = '/static'
-export const APP_NAME = 'Boilerplate 1 Javascript Application'
+export const APP_NAME = 'Boilerplate Application'
